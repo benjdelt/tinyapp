e9b4c67599313baad08e44c099c1341e82a04b95
Ben Deltenre <benjdelt@gmail.com>
2018-08-08 22:37:58 +0000

    Added register view and database

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,46 @@
 const express = require('express');
 const usersRouter = new express.Router();
 
+// Simulate Database
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+
 // Routes
 
 usersRouter
 
-// Login user
+  // Create user
 
-.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect(303, '/urls');
-})
+  .get('/register', (req, res) => {
+    res.render('register');
+  })
+
+  // Login user
+
+  .post('/login', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect(303, '/urls');
+  })
 
 
-// Logout user
+  // Logout user
 
-.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect(303, '/urls');
-});
+  .post("/logout", (req, res) => {
+    res.clearCookie('username');
+    res.redirect(303, '/urls');
+  });
 
 
 module.exports = usersRouter;
